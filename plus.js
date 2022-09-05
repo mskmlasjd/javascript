82d9031fe178651a51dc3f05c73f957be654556e
mskmlasjd <102935614+mskmlasjd@users.noreply.github.com>
2022-09-05 11:35:22 +0900

    Add files via upload
    9월5일

---
+++ plus.js
@@ -0,0 +1,20 @@
+function plus(num1,num2){
+    let result;
+    result=num1+num2;
+    return result
+}
+function minus(num1,num2){
+    let result;
+    result=num1-num2;
+    return result
+}
+function divide(num1,num2){
+    let result;
+    result=num1/num2;
+    return result
+}
+function multiply(num1,num2){
+    let result;
+    result=num1*num2;
+    return result
+}
